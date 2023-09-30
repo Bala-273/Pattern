a146a75ad0aea701a81739e2517868d7cb7d3f93
Balamurugan K <138365153+Bala-273@users.noreply.github.com>
2023-09-30 12:32:08 +0530

    Add files via upload

---
+++ Pattern.py
@@ -0,0 +1,12 @@
+def num(n):
+    x=1
+    for i in range(0,n):
+        
+        for j in range(0,i+1):
+            print('x',end=' ')
+            x+=1
+        print('')
+n=5
+num(n)
+    
+       
